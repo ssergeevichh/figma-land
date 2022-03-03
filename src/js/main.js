3067db3a0c955e7149242675f25222932b87d52f
Vladyslav Kuidin <vkuydi@gmail.com>
2022-03-03 19:36:08 +0200

    feat:next sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
-import '../styles/main.scss'
+import '../styles/main.scss';
 
-const hamburger = document.querySelector('.hamburger')
-const closeBtn = document.querySelector('.mobile-close')
-const menu = document.querySelector('.menu')
-const menuOverlay = document.querySelector('.menu-overlay')
+const hamburger = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.mobile-close');
+const menu = document.querySelector('.menu');
+const menuOverlay = document.querySelector('.menu-overlay');
+const playBtn = document.querySelector('.play-button-bg');
+const video = document.querySelector('.video');
+const windowExtension = window.matchMedia("(max-width: 575px)");
+let isPaused;
 
 hamburger.addEventListener('click', function () {
     menu.classList.add('menu__active')
@@ -11,7 +15,9 @@ hamburger.addEventListener('click', function () {
     hamburger.style.display = 'none'
 
 })
+
 document.addEventListener('click', function ({ target }) {
+    console.log(target);
     if (target === closeBtn || target.tagName === 'SPAN' || target === menuOverlay) {
         menu.classList.remove('menu__active')
         menuOverlay.style.display = ''
@@ -19,3 +25,35 @@ document.addEventListener('click', function ({ target }) {
         setTimeout(() => hamburger.style.display = 'flex', 200)
     }
 })
+
+
+
+if (windowExtension.matches) {
+    video.setAttribute('poster', '/img/mobile-laptop.png')
+}
+
+playBtn.addEventListener('click', () => {
+    video.setAttribute('controls', 'controls')
+    playBtn.style.display = 'none'
+    video.play()
+
+    isPaused = false
+    
+    if (isPaused) {
+        playBtn.style.display = 'block'
+    }
+
+})
+
+video.addEventListener('click', () => {
+    isPaused = !isPaused
+
+    if (isPaused) {
+        playBtn.style.display = 'block'
+    } else {
+        playBtn.style.display = 'none'
+    }
+
+})
+
+
